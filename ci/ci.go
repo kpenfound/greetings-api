6dab4f19a30f7778c8b107dd3aaee52a87e1c103
kpenfound <kyle@dagger.io>
2023-01-11 23:14:45 -0500

    demo pr
    Signed-off-by: kpenfound <kyle@dagger.io>

---
+++ ci/ci.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"dagger.io/dagger"
+)
+
+func main() {
+	ctx := context.Background()
+	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stdout))
+	if err != nil {
+		panic(err)
+	}
+	// get project dir
+	src := client.Host().Directory(".")
+
+	err = nix(ctx, client, src)
+	if err != nil {
+		panic(err)
+	}
+
+	err = docs(ctx, client, src)
+	if err != nil {
+		panic(err)
+	}
+
+	err = test(ctx, client, src)
+	if err != nil {
+		panic(err)
+	}
+
+	err = build(ctx, client, src)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func nix(ctx context.Context, client *dagger.Client, src *dagger.Directory) error {
+	err := client.Container().
+		From("alpine").
+		Pipeline("nix").
+		WithMountedDirectory("/src", src).
+		WithWorkdir("/src").
+		WithExec([]string{"make", "nix"}).
+		ExitCode(ctx)
+
+	return err
+}
+
+func docs(ctx context.Context, client *dagger.Client, src *dagger.Directory) error {
+	err := client.Container().
+		From("alpine").
+		Pipeline("docs").
+		WithMountedDirectory("/src", src.directory("docs")).
+		WithWorkdir("/src").
+		WithExec([]string{"make", "docs"}).
+		ExitCode(ctx)
+
+	return err
+}
+
+func test(ctx context.Context, client *dagger.Client, src *dagger.Directory) error {
+	err := client.Container().
+		From("golang:latest").
+		Pipeline("test").
+		WithMountedDirectory("/src", src).
+		WithWorkdir("/src").
+		WithExec([]string{"go", "test"}).
+		ExitCode(ctx)
+
+	return err
+}
+
+var platforms = []string{"windows", "linux", "darwin"}
+
+func build(ctx context.Context, client *dagger.Client, src *dagger.Directory) error {
+	buildoutput := client.Directory()
+
+	for _, plat := range platforms {
+		pipeline := fmt.Sprintf("build-%s", plat)
+		builder := client.Container().
+			From("golang:latest").
+			Pipeline(pipeline).
+			WithMountedDirectory("/src", src).
+			WithWorkdir("/src").
+			WithEnvVariable("GOOS", plat).
+			WithExec([]string{"go", "build", "-o", "/src/greetings-api"})
+
+		// Get Command Output
+		outputfile := fmt.Sprintf("output/%s/greetings-api", string(plat))
+		buildoutput = buildoutput.WithFile(
+			outputfile,
+			builder.File("/src/greetings-api"),
+		)
+	}
+
+	_, err := buildoutput.Export(ctx, ".")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
