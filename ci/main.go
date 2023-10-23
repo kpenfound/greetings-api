d511a524a3f3e770df4c6dc745cc7d0d968f4f2b
Kyle Penfound <kyle@dagger.io>
2023-10-23 14:47:17 -0400

    update dx for new golang module
    Signed-off-by: Kyle Penfound <kyle@dagger.io>

--- ci/main.go
+++ ci/main.go
@@ -4,40 +4,25 @@ import "context"
 
 type Greetings struct{}
 
-func (m *Greetings) HelloWorld(ctx context.Context) (string, error) {
-	return dag.Golang().Base("latest").WithExec([]string{"echo", "hello", "world"}).Stdout(ctx)
-}
-
-func (g *Greetings) Binary(ctx context.Context) (*File, error) {
-	d, err := g.Build(ctx)
-	if err != nil {
-		return nil, err
-	}
-	return d.File("greetings-api"), nil
+func (g *Greetings) Binary(ctx context.Context) *File {
+	d := g.Build(ctx)
+	return d.File("greetings-api")
 }
 
 func (g *Greetings) UnitTest(ctx context.Context) (string, error) {
-	base := dag.
-		Golang().Base("1.21").
-		WithDirectory("/src", project()).
-		WithWorkdir("/src")
-	b := dag.Golang().Test(base, []string{"./..."})
-
-	return b.Stdout(ctx)
+	return dag.
+		Golang().
+		WithProject(project()).
+		Test([]string{"./..."}).
+		Container().Stdout(ctx)
 }
 
-// func (g *Greetings) Lint(ctx context.Context) (string, error) {
-// 	return project().GoLint(ctx)
-// }
-
-func (g *Greetings) Build(ctx context.Context) (*Directory, error) {
-	base := dag.
-		Golang().Base("1.21").
-		WithDirectory("/src", project()).
-		WithWorkdir("/src")
-	b := dag.Golang().Build(base, []string{})
-
-	return b.Directory("/src"), nil
+func (g *Greetings) Build(ctx context.Context) *Directory {
+	return dag.
+		Golang().
+		WithProject(project()).
+		Build([]string{}).
+		Project()
 }
 
 func project() *Directory {
