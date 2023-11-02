b7fa36b3216b4fda0409a408c882f3cb49ab97a3
Kyle Penfound <kyle@dagger.io>
2023-11-02 17:17:17 -0400

    add ci function
    Signed-off-by: Kyle Penfound <kyle@dagger.io>

--- ci/main.go
+++ ci/main.go
@@ -5,7 +5,12 @@ import (
 	"fmt"
 )
 
+const (
+	REPO = "github.com/kpenfound/greetings-api"
+)
+
 type Greetings struct{}
+
 
 func (g *Greetings) UnitTest(ctx context.Context, dir *Directory) (string, error) {
 	backendResult, err := dag.Backend().UnitTest(ctx, dir)
@@ -40,6 +45,22 @@ func (g *Greetings) Serve(dir *Directory) *Service {
 	Service()
 }
 
+func (g *Greetings) Release(ctx context.Context, dir *Directory, tag string, ghToken *Secret) (string, error) {
+	// Get build
+	build := g.Build(dir, "netlify")
+	// Compress frontend build
+	assets := dag.Container().From("alpine:3.18").
+	WithDirectory("/build", build).
+	WithWorkdir("/build").
+	WithExec([]string{"tar", "czf", "website.tar.gz", "website/"}).
+	WithExec([]string{"rm", "-r", "website"}).
+	Directory("/build")
+
+	title := fmt.Sprintf("Release %s", tag)
+
+	return dag.GithubRelease().Create(ctx, REPO, tag, title, ghToken, GithubReleaseCreateOpts{ Assets: assets })
+}
+
 func (g *Greetings) Deploy(ctx context.Context, dir *Directory, flyToken *Secret, netlifyToken *Secret) (string, error) {
 	// Backend
 	imageTag := "kylepenfound/greetings-api:latest"
@@ -65,6 +86,51 @@ func (g *Greetings) Deploy(ctx context.Context, dir *Directory, flyToken *Secret
 		return "", err
 	}
 	return fmt.Sprintf("BACKEND\n\n%s\n\nFRONTEND\n\n%s", backendResult, frontendResult), nil
+}
+
+func (g *Greetings) Ci(
+	ctx context.Context,
+	dir *Directory,
+	release Optional[bool],
+	tag Optional[string],
+	flyToken Optional[*Secret],
+	netlifyToken Optional[*Secret],
+	ghToken Optional[*Secret],
+) (string, error) {
+	out, err := g.Lint(ctx, dir)
+	if err != nil {
+		return "", err
+	}
+	testOut, err := g.UnitTest(ctx, dir)
+	if err != nil {
+		return "", err
+	}
+	out = out + "\n" + testOut
+
+	if release.GetOr(false) {
+		tag_, tagSet := tag.Get()
+		ghToken_, ghSet := ghToken.Get()
+
+		if tagSet && ghSet {
+			releaseOut, err := g.Release(ctx, dir, tag_, ghToken_)
+			if err != nil {
+				return "", err
+			}
+			out = out + "\n" + releaseOut
+		}
+		fly, flySet := flyToken.Get()
+		netlify, netlifySet := netlifyToken.Get()
+
+		if flySet && netlifySet {
+			deployOut, err := g.Deploy(ctx, dir, fly, netlify)
+			if err != nil {
+				return "", err
+			}
+			out = out + "\n" + deployOut
+		}
+	}
+
+	return out, nil
 }
 
 func fly_deploy(ctx context.Context, imageRef string, token *Secret) (string, error) {
