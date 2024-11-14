97aa514ccef78fbe2963ae66b1dcffecbb7af885
Kyle Penfound <kyle@dagger.io>
2024-11-14 13:43:19 -0500

    update greetings-api dagger module

--- .dagger/backend/main.go
+++ .dagger/backend/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"context"
 	"runtime"
+
+	"backend/internal/dagger"
 )
 
 type Backend struct{}
 
 // Run the unit tests for the backend
-func (b *Backend) UnitTest(ctx context.Context, source *Directory) (string, error) {
+func (b *Backend) UnitTest(ctx context.Context, source *dagger.Directory) (string, error) {
 	return dag.
 		Golang().
 		WithProject(source).
@@ -16,7 +18,7 @@ func (b *Backend) UnitTest(ctx context.Context, source *Directory) (string, erro
 }
 
 // Lint the backend Go code
-func (b *Backend) Lint(ctx context.Context, source *Directory) (string, error) {
+func (b *Backend) Lint(ctx context.Context, source *dagger.Directory) (string, error) {
 	return dag.
 		Golang().
 		WithProject(source).
@@ -25,41 +27,41 @@ func (b *Backend) Lint(ctx context.Context, source *Directory) (string, error) {
 
 // Build the backend
 func (b *Backend) Build(
-	source *Directory,
+	source *dagger.Directory,
 	// +optional
 	arch string,
-) *Directory {
+) *dagger.Directory {
 	if arch == "" {
 		arch = runtime.GOARCH
 	}
 	return dag.
 		Golang().
 		WithProject(source).
-		Build([]string{}, GolangBuildOpts{Arch: arch})
+		Build([]string{}, dagger.GolangBuildOpts{Arch: arch})
 }
 
 // Return the compiled backend binary for a particular architecture
 func (b *Backend) Binary(
-	source *Directory,
+	source *dagger.Directory,
 	// +optional
 	arch string,
-) *File {
+) *dagger.File {
 	d := b.Build(source, arch)
 	return d.File("greetings-api")
 }
 
 // Get a container ready to run the backend
 func (b *Backend) Container(
-	source *Directory,
+	source *dagger.Directory,
 	// +optional
 	arch string,
-) *Container {
+) *dagger.Container {
 	if arch == "" {
 		arch = runtime.GOARCH
 	}
 	bin := b.Binary(source, arch)
 	return dag.
-		Container(ContainerOpts{Platform: Platform(arch)}).
+		Container(dagger.ContainerOpts{Platform: dagger.Platform(arch)}).
 		From("cgr.dev/chainguard/wolfi-base:latest@sha256:a8c9c2888304e62c133af76f520c9c9e6b3ce6f1a45e3eaa57f6639eb8053c90").
 		WithFile("/bin/greetings-api", bin).
 		WithEntrypoint([]string{"/bin/greetings-api"}).
@@ -67,6 +69,6 @@ func (b *Backend) Container(
 }
 
 // Get a Service to run the backend
-func (b *Backend) Serve(source *Directory) *Service {
+func (b *Backend) Serve(source *dagger.Directory) *dagger.Service {
 	return b.Container(source, runtime.GOARCH).AsService()
 }
