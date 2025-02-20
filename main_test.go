d73df836b489322ce84d22cda3fb53d46bb3a03f
Marvin <marvin@dagger.io>
2025-02-20 05:01:17 +0000

    'Add test for new greeting function'

--- main_test.go
+++ main_test.go
@@ -12,3 +12,10 @@ func TestGreeting(t *testing.T) {
 
 	assert.Equal(t, should, g)
 }
+
+func TestFrenchGreeting(t *testing.T) {
+	g := frenchGreeting()
+	should := "{\"greeting\":\"Salut les Daggernauts!\"}"
+
+	assert.Equal(t, should, g)
+}
