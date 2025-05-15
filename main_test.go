5a8bba73d7f72895cb40c8add9aa5ef27621aae1
Dagger <noreply@dagger.io>
2025-05-15 03:37:57 +0000

    feat_add_endpoint_to_create_greetings_1747262834

--- main_test.go
+++ main_test.go
@@ -11,7 +11,49 @@ import (
 	"testing"
 
 	"github.com/gorilla/mux"
+	"gotest.tools/v3/assert"
 )
+
+func TestSelectGreeting(t *testing.T) {
+	var greetings []*Greeting
+	err := json.Unmarshal(greetingsJson, &greetings)
+	if err != nil {
+		fmt.Printf("error loading greetings: %s\n", err)
+		os.Exit(1)
+	}
+
+	english := &Greeting{
+		Greeting: "Hello, World!",
+		Language: "english",
+	}
+
+	// Test with a language
+	g, err := SelectGreeting(greetings, "english")
+	assert.NilError(t, err)
+	assert.Equal(t, *english, *g)
+
+	// Test random
+	_, err = SelectGreeting(greetings, "random")
+	assert.NilError(t, err)
+
+	// Test invalid language
+	_, err = SelectGreeting(greetings, "foooooo")
+	assert.Error(t, err, "no greeting found for language 'foooooo'")
+
+	// Test empty language
+	_, err = SelectGreeting(greetings, "")
+	assert.Error(t, err, "no greeting found for language ''")
+}
+
+func TestFormatResponse(t *testing.T) {
+	g := &Greeting{
+		Greeting: "Hello, World!",
+		Language: "english",
+	}
+
+	formatted := FormatResponse(g)
+	assert.Equal(t, "{\"greeting\":\"Hello, World!\"}", formatted)
+}
 
 func TestAddGreetingEndpoint(t *testing.T) {
 	err := os.Setenv("ADMIN_API_KEY", "testapikey")
